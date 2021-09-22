55a247e2ee0630d8a7d09ab1d6640f641f75aa1c
Therese Stokkan <thestokkan@gmail.com>
2021-09-22 13:25:25 +0200

    deleted temp files

--- lesson_2_small_programs/test_pry.rb
+++
@@ -1,9 +0,0 @@
-require "pry"
-
-counter = 0
-
-loop do
-  counter += 1
-  binding.pry
-  break if counter == 5
-end
--- lesson_2_small_programs/test_yaml.rb
+++
@@ -1,5 +0,0 @@
-require 'yaml'
-
-MESSAGES = YAML.load_file('calc_messages_en.yml')
-
-puts MESSAGES.inspect
